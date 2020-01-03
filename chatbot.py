f12fa287671d7f06dff38fdeb5f42e9407b34632
wilson <wilson.p17@iiits.in>
2020-01-03 17:44:38 +0530

    Added the seperate terminals

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,11 @@
 import random
+import sys
+import os
+import platform
+from subprocess import Popen
 from human import chat
+from test_code import run_foo2
+
 # A Dictionary of queries with a list of choices for the bot to answer differently every time
 responses={'hi':['Hello!','Hola!'],'how are you':['I am fine. Thank You',"I am doing well"],'who are you':['I am a very basic rule based chatbot designed to chat with you for a while.'],'what do you like':['You want to know about me. That is so sweet of you. Are you interested in me?'],'no':['Sad to hear that. Anyways I like to learn but I am unable to.'],'yes':['Very well. I like to learn but I am unable to.'],'why':['I am not designed to learn anything new. Anyways you want to ask something else?'],'suggest me some movies':['Do watch Endgame','I would recommend Shawshank Redemption','You can watch The Pulp Fiction'],'i have already watched it':['There are many more movies like The forrest Gump and Good Will Hunting to watch','You can also watch Interstellar','Then I would recommend The Shutter Island'],'who is your creator':['Wilson Patro created me. He should have made me more intelligent.'],'ok i will watch it':['It is nice to help you. Is there anything else you wanna ask me?'],'how is the weather today':['It is a sunny day','It might rain according to the weather report'],'bye':['It was nice talking to you. Bye!','Ok, come back soon. Bye!','See you later. Bye','All right then. Bye!!']}
 common=['yes','no','why']
@@ -10,8 +16,20 @@ counter={}
 for i in responses:
 	counter[i.lower().strip('?').strip('.')]=0
 # Function for the human support to come and chat with the customer
+def run_foo2():
+    # print("foo2 >>> Something")
+    return "import sys; sys.path.append('path_to_your_program_folder'); from human import chat; chat()"
 
+def new1():
+	echos = [ [sys.executable, "-c",run_foo2()]  ]    
+	processes = [Popen(new_window_command + echo) for echo in echos]
 # A Function for the chatbot
+def run_foo1():
+    # print("foo1 >>> Something")
+    return "import sys; sys.path.append('path_to_your_program_folder'); from chatbot import bot; bot()"
+
+
+
 def bot():
 	#Initial Instructions
 	B="Hello!"
@@ -28,7 +46,7 @@ def bot():
 			break
 		if W=='1': # After confirming the query is correct press 1 for the customer support
 			print("I think it is something which I cannot answer. Let me ask for help from someone")
-			W=chat()
+			W=new1()
 			if W=='':
 				print("Bot: Hope your doubt is cleared. Bye!!")
 				break
@@ -55,4 +73,18 @@ def bot():
 		if W.lower() not in responses: # Checking if the query doesn't contain any grammatical mistakes before going to the human support
 			print("Bot: Kindly check for any error in your query. If it's perfectly fine please press 1")
 
-bot()
+if platform.system() == "Windows":
+    new_window_command = "cmd.exe /c start".split()
+else:  #XXX this can be made more portable
+    new_window_command = "x-terminal-emulator -e".split()
+if __name__ == '__main__':
+
+    # open new consoles, display messages
+    # bot()
+    echos = [[sys.executable, "-c",run_foo1()]]    
+    processes = [Popen(new_window_command + echo) for echo in echos]
+    # print(processes)
+
+    # wait for the windows to be closed
+    for proc in (processes):
+        proc.wait()
